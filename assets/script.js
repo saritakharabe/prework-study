b73a07c79fa2ed483233c618cbded8da4b5c091c
Sarita Kharabe <98185682+saritakharabe@users.noreply.github.com>
2023-03-01 20:05:38 -0500

    added control flow statement

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
 var topics = ["HTML", "CSS", "Javascript", "Git"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(var i=0; i< topics.length; i++){
-    console.log(topics[i]);
+function listTopics(){
+    for(var i=0; i< topics.length; i++){
+        console.log(topics[i]);
+    }
 }
 
-/* if(topic === "HTML"){
-    console.log("let's study HTML");
-}else if( topic === "CSS"){
-    console.log("let's study CSS");
-}else if( topic === "Javascript"){
-    console.log("lets study javascript");
-}else if(topic === "Git"){
-    console.log("let's study git");
-}else{
-    console.log("Please try again");
+function selectTopic(){
+    if(randomTopic === "HTML"){
+        console.log("let's study HTML");
+    }else if( randomTopic === "CSS"){
+        console.log("let's study CSS");
+    }else if( randomTopic === "Javascript"){
+        console.log("lets study javascript");
+    }else if(randomTopic === "Git"){
+        console.log("let's study git");
+    }else{
+        console.log("Please try again");
+    }
 }
-*/
+ 
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
